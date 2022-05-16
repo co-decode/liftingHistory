1c4376075046d9392098ab3a0ba160be445ceb59
code <codecode60@protonmail.com>
2022-05-16 13:05:45 +0930

    first commit

---
+++ restapi/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const studentRoutes = require('./src/student/routes');
+const app = express();
+const port = 3001;
+
+const cors = require('cors');
+
+app.use(
+    cors({
+        origin: "http://localhost:3000"
+    })
+)
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.use('/api/v1/students', studentRoutes);
+
+app.listen(port, () => console.log(`app listening on port ${port}`))
