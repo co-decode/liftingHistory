0a178b6979271178d3a280f252c320f085ee9b18
code <codecode60@protonmail.com>
2022-05-18 13:47:47 +0930

    Modified REST functions

--- restapi/src/session/controller.js
+++ restapi/src/session/controller.js
@@ -12,8 +12,8 @@ const getSessions = (req, res) => {
   pool.query(queries.getSessions, (error, results) => {
     if (error) throw error;
     res.status(200).json(results.rows);
-  })
-}
+  });
+};
 
 const getStudentById = (req, res) => {
   const id = parseInt(req.params.id);
@@ -23,8 +23,8 @@ const getStudentById = (req, res) => {
   });
 };
 
-const getSessionById = ( req, res ) => {
-  const sessionNumber = parseInt(req.params.sessionNumber)
+const getSessionById = (req, res) => {
+  const sessionNumber = parseInt(req.params.sessionNumber);
   pool.query(queries.getSessionById, [sessionNumber], (error, results) => {
     if (error) throw error;
     res.status(200).json(results.rows);
@@ -36,7 +36,7 @@ const addStudent = (req, res) => {
   pool.query(queries.checkEmailExists, [email], (error, results) => {
     if (results.rows.length) {
       res.send("Email already exists.");
-      return
+      return;
     }
 
     pool.query(
@@ -50,39 +50,132 @@ const addStudent = (req, res) => {
   });
 };
 
-const removeStudent = (req, res) => {
-    const id = parseInt(req.params.id);
-    
-    pool.query(queries.getStudentById, [id], (error,results) => {
-        const noStudentFound = !results.rows.length;
-        if (noStudentFound) {
-            res.send("Student does not exist in the database");
-        }
+const addSession = (req, res) => {
+  const {
+    date,
+    alias,
+    horizontal_press,
+    vertical_press,
+    vertical_pull,
+    elbow_flexion,
+    knee_flexion,
+    hip_hinge,
+  } = req.body;
 
-        pool.query(queries.removeStudent, [id], (error, results) => {
-            if (error) throw error;
-            res.status(200).send("Student removed successfully.");
-        })
-    })
-}
+  pool.query(queries.checkSessionDate, [date], (error, results) => {
+    if (results.rows.length) {
+      res.send("A session for this date has already been recorded.");
+      return;
+    }
+
+    pool.query(
+      queries.addSession,
+      [
+        date,
+        alias,
+        horizontal_press,
+        vertical_press,
+        vertical_pull,
+        elbow_flexion,
+        knee_flexion,
+        hip_hinge,
+      ],
+      (error, results) => {
+        if (error) throw error;
+        return res.status(201).send("Session recorded successfully!");
+      }
+    );
+  });
+};
+
+const removeStudent = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    const noStudentFound = !results.rows.length;
+    if (noStudentFound) {
+      res.send("Student does not exist in the database");
+    }
+
+    pool.query(queries.removeStudent, [id], (error, results) => {
+      if (error) throw error;
+      res.status(200).send("Student removed successfully.");
+    });
+  });
+};
+
+const removeSession = (req, res) => {
+  const sessionNumber = parseInt(req.params.sessionNumber);
+  pool.query(queries.getSessionById, [sessionNumber], (error, results) => {
+    const noSessionFound = !results.rows.length;
+    if (noSessionFound) {
+      return res.send("No session is recorded for that date");
+    }
+
+    pool.query(queries.removeSession, [sessionNumber], (error, results) => {
+      if (error) throw error;
+      return res.status(200).send("Session removed successfully.");
+    });
+  });
+};
 
 const updateStudent = (req, res) => {
-    const id = parseInt(req.params.id);
-    const { name } = req.body;
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
 
-    pool.query(queries.getStudentById, [id], (error, results) => {
-        const noStudentFound = !results.rows.length;
-        if ( noStudentFound) {
-            res.send("Student does not exist in database");
-        }
+  pool.query(queries.getStudentById, [id], (error, results) => {
+    const noStudentFound = !results.rows.length;
+    if (noStudentFound) {
+      res.send("Student does not exist in database");
+    }
 
-        pool.query(queries.updateStudent, [name, id], (error, results) => {
-            if (error) throw error;
-            res.status(200).send("Student updated successfully");
-        })
-    })
+    pool.query(queries.updateStudent, [name, id], (error, results) => {
+      if (error) throw error;
+      res.status(200).send("Student updated successfully");
+    });
+  });
+};
 
-}
+const updateSession = (req, res) => {
+  const sessionNumber = parseInt(req.params.sessionNumber);
+  const {
+    date,
+    alias,
+    horizontal_press,
+    vertical_press,
+    vertical_pull,
+    elbow_flexion,
+    knee_flexion,
+    hip_hinge,
+  } = req.body;
+
+  pool.query(queries.getSessionById, [sessionNumber], (error, results) => {
+    const noSessionFound = !results.rows.length;
+    if (noSessionFound) {
+      res.send("Session does not exist in database");
+      return;
+    }
+
+    pool.query(
+      queries.updateSession,
+      [
+        date,
+        alias,
+        horizontal_press,
+        vertical_press,
+        vertical_pull,
+        elbow_flexion,
+        knee_flexion,
+        hip_hinge,
+        sessionNumber,
+      ],
+      (error, results) => {
+        if (error) throw error;
+        res.status(200).send("Session updated successfully");
+      }
+    );
+  });
+};
 
 module.exports = {
   getStudents,
@@ -90,6 +183,9 @@ module.exports = {
   getStudentById,
   getSessionById,
   addStudent,
+  addSession,
   removeStudent,
+  removeSession,
   updateStudent,
+  updateSession
 };
--- restapi/src/session/routes.js
+++ restapi/src/session/routes.js
@@ -6,12 +6,16 @@ const router = Router();
 // router.get('/', controller.getStudents);
 router.get('/', controller.getSessions);
 // router.post('/', controller.addStudent);
-// router.post('/', controller.addSession);
+router.post('/', controller.addSession);
 
 // router.get('/:id', controller.getStudentById);
 router.get('/:sessionNumber', controller.getSessionById);
 
-router.put('/:id', controller.updateStudent)
-router.delete('/:id', controller.removeStudent);
+// router.put('/:id', controller.updateStudent)
+router.put('/:sessionNumber', controller.updateSession)
+
+
+// router.delete('/:id', controller.removeStudent);
+router.delete('/:sessionNumber', controller.removeSession);
 
 module.exports = router;
