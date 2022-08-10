273b499ecce679da1737786c1384fd7e1b29e0e0
code <codecode60@protonmail.com>
2022-08-10 10:36:33 +0930

    finished with Tonnage, working on breakdown

--- Client - Source/src/Tonnage.js
+++ Client - Source/src/Tonnage.js
@@ -3,19 +3,20 @@ import returnTonnage from "./utils/tonnageFunctions";
 
 
 export default function Tonnage({get}) {
-    const [format, setFormat] = useState(null)
+    // const [format, setFormat] = useState(null)
     const [interval, setInterval] = useState(null)
+    const [intervalLength, setIntervalLength] = useState([null, null])
+    const [referenceDate, setReferenceDate] = useState(null)
     const customInput = useRef()
     return(
         <>
-        <button onClick={()=> console.log(format, interval)}>log</button>
         <fieldset>
-            <label htmlFor="tonnageFormat">Tonnage Format</label>
+           {/*  <label htmlFor="tonnageFormat">Tonnage Format</label>
             <select id="tonnageFormat" onChange={(e)=>setFormat(e.target.value)}>
                 <option value="">___</option>
                 <option value={"REPS"}>reps</option>
                 <option value={"MASS"}>mass</option>
-            </select>
+            </select> */}
             <label htmlFor="tonnageInterval">Interval</label>
             <select id="tonnageInterval" ref={customInput} onChange={(e)=>setInterval(e.target.value)}>
                 <option value="">___</option>
@@ -25,15 +26,21 @@ export default function Tonnage({get}) {
                 <option value="ALL">all</option>
                 <option value="CUSTOM">custom</option>
             </select>
-            {customInput.current?.value === "CUSTOM" &&
+            {interval === "CUSTOM" &&
             <>
-                <label>Interval Length</label>
-                <input />
-                <label>Beginning Reference Date</label>
-                <input />
+                <label htmlFor={`IntervalLength`}>Interval Length</label>
+                <input id={`IntervalLength`} onChange={(e)=> intervalLength[1] === "WEEKS" ? setIntervalLength([parseInt(e.target.value) * 7, intervalLength[1]]) : setIntervalLength([parseInt(e.target.value), intervalLength[1]])}/>
+                <label htmlFor={`intervalLengthIn...`}>in...</label>
+                <select id={`intervalLengthIn...`} onChange={(e)=> setIntervalLength([intervalLength[0], e.target.value])}>
+                    <option value=""> --- </option>
+                    <option value="DAYS">DAYS</option>
+                    <option value="WEEKS">WEEKS</option>
+                </select>
+                <label htmlFor={`BeginReferenceDate`}>Beginning Reference Date</label>
+                <input id={`BeginReferenceDate`} type="date" onChange={(e)=> setReferenceDate(e.target.value)}/>
             </>}
         </fieldset>
-        {returnTonnage(get, format, interval)}
+        {returnTonnage(get, /* format, */ interval || "ALL", intervalLength[0] || 7, referenceDate)}
         </>
     )
 }
--- Client - Source/src/utils/tonnageFunctions.js
+++ Client - Source/src/utils/tonnageFunctions.js
@@ -1,111 +1,111 @@
 import { exerciseArray } from "./variables";
 
-const [reps, mass, session, custom, month, all, week] = [
-  "REPS",
-  "MASS",
+const [session, custom, month, all, week] = [
   "SESSION",
   "CUSTOM",
   "MONTH",
   "ALL",
-  "WEEK"
+  "WEEK",
 ];
 
-export default function returnTonnage(get, tonFormat, interval, intervalLengthInDays, beginning) {
-  if (![reps, mass].includes(tonFormat)) return null;
-  else if (interval === session) 
-    return sessionTon(get, tonFormat);
-  else if ([custom, month, all, week].includes(interval)) 
-    return intervalTon(get, tonFormat, interval, intervalLengthInDays = 7, beginning);
-  /* else if (interval === week) return weekTon(get, tonFormat);
-  else if (interval === month) return monthTon(get, tonFormat);
-  else if (interval === all) return totalTon(get, tonFormat); */
+export default function returnTonnage(
+  get,
+  // tonFormat,
+  interval,
+  intervalLengthInDays,
+  beginning
+) {
+  // if (![reps, mass].includes(tonFormat)) return null;
+  if ([session, custom, month, all, week].includes(interval))
+    return intervalTon(
+      get,
+      // tonFormat,
+      interval,
+      intervalLengthInDays || 7,
+      beginning
+    );
+  // else if (interval === session) return sessionTon(get, tonFormat);
 }
 
-function sessionTon(get, tonFormat) {
-  return (
-    <>
-      <div>
-        - Total Reps -{" "}
-        {exerciseArray.map((exercise) => {
-          const sidList = get.date.filter((val) =>
-            val.exercises.includes(exercise)
-          );
-          return (
-            <div key={`${exercise}${tonFormat}`}>
-              {" "}
-              {exercise[0].toUpperCase() + exercise.slice(1) + ": "}
-              {sidList
-                .map((sidV) => [
-                  get[exercise].filter((val) => val.sid === sidV.sid),
-                  sidV.date,
-                ])
-                .map((v) => {
-                  let addition;
-                  let session = v[0][0];
-                  if (tonFormat === reps) {
-                    addition = session.reps.reduce(
-                      (acc, val) => (acc += val),
-                      0
-                    );
-                  } else if (tonFormat === mass) {
-                    addition = session.reps.reduce(
-                      (acc, val, ind) => (acc += val * session.mass[ind]),
-                      0
-                    );
-                  }
-                  return (
-                    <div key={`${v[1]}_${addition}`}>
-                      {tonFormat[0] + tonFormat.slice(1).toLowerCase()}:{" "}
-                      {addition} {tonFormat === mass && " kg"}
-                      {" - "}
-                      {new Date(v[1]).toDateString()}
-                    </div>
-                  );
-                })}
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
-}
-
-/* function weekTon(get, tonFormat) {
-  function getWeek(date) {
+function intervalTon(
+  get,
+  // tonFormat,
+  intervalFormat,
+  intervalLengthInDays,
+  begin
+) {
+  function getCustomInterval(date) {
     const time = new Date(date);
     const onejan = new Date(time.getFullYear(), 0, 1);
-    const week = Math.ceil(
-      ((time.getTime() - onejan.getTime()) / 86400000 + onejan.getDay() + 1) / 7
+    const referenceDate = begin ? new Date(begin) : onejan;
+    const interval = Math.ceil(
+      (time.getTime() - referenceDate.getTime()) /
+        86400000 /
+        intervalLengthInDays
     );
-    return week;
+    return interval;
   }
-  const sidsBundled = get.date.map((v) => {
-    return {
-      sid: v.sid,
-      week: `${getWeek(v.date)}-${new Date(v.date).getFullYear()}`,
-    };
-  });
-  const sidsBinned = sidsBundled.reduce((acc, v) => {
-    return Object.keys(acc).includes(v.week)
-      ? { ...acc, [v.week]: [...acc[v.week], v.sid] }
-      : { ...acc, [v.week]: [v.sid] };
+  function returnInterval(date) {
+    if (intervalFormat === "CUSTOM") {
+      return begin
+        ? `${getCustomInterval(date)}`
+        : `${getCustomInterval(date)}-${new Date(date).getFullYear()}`;
+    } else if (intervalFormat === week)
+      return `${getCustomInterval(date)}-${new Date(date).getFullYear()}`;
+    else if (intervalFormat === month)
+      return `${new Date(date).getMonth() + 1}-${new Date(date).getFullYear()}`;
+    else if (intervalFormat === all) return `ALL`;
+    else if (intervalFormat === session)
+      return `${new Date(date).toDateString()}`;
+  }
+  const sidsTagged = get.date
+    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+    .map((v) => {
+      return {
+        sid: v.sid,
+        interval: returnInterval(v.date),
+      };
+    });
+  const sidsBinned = sidsTagged.reduce((acc, v) => {
+    return Object.keys(acc).includes(v.interval)
+      ? { ...acc, [v.interval]: [...acc[v.interval], v.sid] }
+      : { ...acc, [v.interval]: [v.sid] };
   }, {});
   return (
     <>
-      {Object.keys(sidsBinned).map((week) => {
+      {exerciseArray.map((exercise) => {
         return (
-          <div key={`${week}`}>
-            <div>{week}</div>
-            <div>
-              {exerciseArray.map((exercise) => {
-                const sidsByExercise = get.date
-                  .filter((val) => sidsBinned[week].includes(val.sid))
-                  .filter((val) => val.exercises.includes(exercise));
+          <div key={`${exercise}`}>
+            {" "}
+            {exercise[0].toUpperCase() + exercise.slice(1) + ": "}
+            {Object.keys(sidsBinned).map((interval) => {
+              const sidsByExercise = get.date
+                .filter((val) => sidsBinned[interval].includes(val.sid))
+                .filter((val) => val.exercises.includes(exercise));
+              if (sidsByExercise.length === 0) return null;
+              const exerciseObjectsForSid = sidsByExercise.map((sidV) =>
+                get[exercise].filter((val) => val.sid === sidV.sid)
+              );
+              const totalReps = exerciseObjectsForSid.reduce((acc, v) => {
+                return acc + v[0].reps.reduce((a, val) => a + val, 0);
+              }, 0);
+              const totalMass = exerciseObjectsForSid.reduce((acc, v) => {
                 return (
-                  <div key={`${exercise}${tonFormat}`}>
-                    {" "}
-                    {exercise[0].toUpperCase() + exercise.slice(1) + ": "}
-                    {sidsByExercise
+                  acc +
+                  v[0].reps.reduce((a, val, ind) => a + val * v[0].mass[ind], 0)
+                );
+              }, 0);
+              return (
+                <div key={`${interval}`}>
+                  {/* <div>{interval}</div> */}
+                  <div>
+                    {/* {`${tonFormat[0]}: `} */}
+                    {`R: `}
+                    {totalReps}
+                    {` | M: `}
+                    {totalMass}
+                    {` kg`}
+                    {/* {sidsByExercise
                       .map((sidV) =>
                         get[exercise].filter((val) => val.sid === sidV.sid)
                       )
@@ -124,97 +124,66 @@ function sessionTon(get, tonFormat) {
                             )
                           );
                         } else return acc;
-                      }, 0)}
-                    {tonFormat === mass && ` kg`}
+                      }, 0)} */}
+                    {/* {tonFormat === mass && ` kg`} */}
+                    {` | Q: `}
+                    {(totalMass / totalReps).toFixed(2)}
+                    {` kg / r | Max: `}
+                    {exerciseObjectsForSid.reduce((intervalMax, v) => {
+                      const sessionMax = v[0].mass.reduce((a, val) => (val > a ? val : a), 0)
+                      return (
+                        sessionMax > intervalMax ? sessionMax : intervalMax
+                      );
+                    }, 0)}
+                    {` kg | `}
+                    {(exerciseObjectsForSid.reduce((acc, v) => {
+                      const sessionReps = v[0].reps.reduce((a, val) => a + val, 0)
+                      const sessionSets = v[0].reps.length
+                      return (
+                        acc + (sessionReps / sessionSets)
+                      );
+                    }, 0) / exerciseObjectsForSid.length).toFixed(2)}
+                    {` r / s | `}
+                    {interval}
                   </div>
-                );
-              })}
-            </div>
+                </div>
+              );
+            })}
           </div>
         );
       })}
     </>
   );
 }
-
-function monthTon(get, tonFormat) {
-  const sidsBundled = get.date.map((v) => {
-    return {
-      sid: v.sid,
-      month: `${new Date(v.date).getMonth() + 1}-${new Date(
-        v.date
-      ).getFullYear()}`,
-    };
-  });
-  const sidsBinned = sidsBundled.reduce((acc, v) => {
-    return Object.keys(acc).includes(v.month)
-      ? { ...acc, [v.month]: [...acc[v.month], v.sid] }
-      : { ...acc, [v.month]: [v.sid] };
-  }, {});
-  return (
-    <>
-      {Object.keys(sidsBinned).map((month) => {
-        return (
-          <div key={`${month}`}>
-            <div>{month}</div>
-            <div>
-              {exerciseArray.map((exercise) => {
-                const sidsByExercise = get.date
-                  .filter((val) => sidsBinned[month].includes(val.sid))
-                  .filter((val) => val.exercises.includes(exercise));
-                return (
-                  <div key={`${exercise}${tonFormat}`}>
-                    {" "}
-                    {exercise[0].toUpperCase() + exercise.slice(1) + ": "}
-                    {sidsByExercise
-                      .map((sidV) =>
-                        get[exercise].filter((val) => val.sid === sidV.sid)
-                      )
-                      .reduce((acc, v) => {
-                        const session = v[0];
-                        if (tonFormat === reps) {
-                          return (
-                            acc + session.reps.reduce((a, val) => a + val, 0)
-                          );
-                        } else if (tonFormat === mass) {
-                          return (
-                            acc +
-                            session.reps.reduce(
-                              (a, val, ind) => a + val * session.mass[ind],
-                              0
-                            )
-                          );
-                        } else return null;
-                      }, 0)}{" "}
-                    {tonFormat === mass && ` kg`}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        );
-      })}
-    </>
-  );
-} */
-
-function intervalTon(get, tonFormat, intervalFormat, intervalLengthInDays, begin) {
+/* THIS function returns interval THEN exercise, current function returns exercise THEN interval.
+function intervalTon(
+  get,
+  tonFormat,
+  intervalFormat,
+  intervalLengthInDays,
+  begin
+) {
   function getCustomInterval(date) {
     const time = new Date(date);
     const onejan = new Date(time.getFullYear(), 0, 1);
-    const referenceDate = begin || onejan
+    const referenceDate = begin ? new Date(begin) : onejan;
     const interval = Math.ceil(
-      (time.getTime() - referenceDate.getTime()) / 86400000 / intervalLengthInDays
+      (time.getTime() - referenceDate.getTime()) /
+        86400000 /
+        intervalLengthInDays
     );
     return interval;
   }
   function returnInterval(date) {
     if (intervalFormat === "CUSTOM") {
-      return begin ? `${getCustomInterval(date)}` : `${getCustomInterval(date)}-${new Date(date).getFullYear()}`;
-    }
-    else if (intervalFormat === week) return `${getCustomInterval(date)}-${new Date(date).getFullYear()}`;
-    else if (intervalFormat === month) return `${new Date(date).getMonth() + 1}-${new Date(date).getFullYear()}`;
-    else if (intervalFormat === all) return `ALL` ;
+      return begin
+        ? `${getCustomInterval(date)}`
+        : `${getCustomInterval(date)}-${new Date(date).getFullYear()}`;
+    } else if (intervalFormat === week)
+      return `${getCustomInterval(date)}-${new Date(date).getFullYear()}`;
+    else if (intervalFormat === month)
+      return `${new Date(date).getMonth() + 1}-${new Date(date).getFullYear()}`;
+    else if (intervalFormat === all) return `ALL`;
   }
   const sidsTagged = get.date.map((v) => {
     return {
@@ -274,12 +243,13 @@ function intervalTon(get, tonFormat, intervalFormat, intervalLengthInDays, begin
     </>
   );
 }
+ */
 
-function totalTon(get, tonFormat) {
+/* function sessionTon(get, tonFormat) {
   return (
     <>
       <div>
-        - {`${tonFormat}`} -{" "}
+        - Total Reps -{" "}
         {exerciseArray.map((exercise) => {
           const sidList = get.date.filter((val) =>
             val.exercises.includes(exercise)
@@ -289,24 +259,33 @@ function totalTon(get, tonFormat) {
               {" "}
               {exercise[0].toUpperCase() + exercise.slice(1) + ": "}
               {sidList
-                .map((sidV) =>
-                  get[exercise].filter((val) => val.sid === sidV.sid)
-                )
-                .reduce((acc, v) => {
-                  const session = v[0];
+                .map((sidV) => [
+                  get[exercise].filter((val) => val.sid === sidV.sid),
+                  sidV.date,
+                ])
+                .map((v) => {
+                  let addition;
+                  let session = v[0][0];
                   if (tonFormat === reps) {
-                    return acc + session.reps.reduce((a, val) => a + val, 0);
-                  } else if (tonFormat === mass) {
-                    return (
-                      acc +
-                      session.reps.reduce(
-                        (a, val, ind) => a + val * session.mass[ind],
-                        0
-                      )
+                    addition = session.reps.reduce(
+                      (acc, val) => (acc += val),
+                      0
                     );
-                  } else return null;
-                }, 0)}
-              {tonFormat === mass && ` kg`}
+                  } else if (tonFormat === mass) {
+                    addition = session.reps.reduce(
+                      (acc, val, ind) => (acc += val * session.mass[ind]),
+                      0
+                    );
+                  }
+                  return (
+                    <div key={`${v[1]}_${addition}`}>
+                      {tonFormat[0] + tonFormat.slice(1).toLowerCase()}:{" "}
+                      {addition} {tonFormat === mass && " kg"}
+                      {" - "}
+                      {new Date(v[1]).toDateString()}
+                    </div>
+                  );
+                })}
             </div>
           );
         })}
@@ -314,3 +293,4 @@ function totalTon(get, tonFormat) {
     </>
   );
 }
+*/
