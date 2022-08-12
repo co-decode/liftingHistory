692864e2ec040fb74d23b44bb6ce52e41bf25c9e
code <codecode60@protonmail.com>
2022-08-13 01:59:40 +0930

    finishing up the chart

--- Client - Source/src/Graph.js
+++ Client - Source/src/Graph.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,7 +10,7 @@ import {
   Legend,
   TimeScale,
 } from "chart.js";
-import 'chartjs-adapter-moment';
+import "chartjs-adapter-moment";
 import { Line } from "react-chartjs-2";
 
 ChartJS.register(
@@ -25,81 +25,266 @@ ChartJS.register(
 );
 
 export default function Graph({ get }) {
-    const totalReps = get.date.filter(sess=>sess.exercises.includes("bench")).map(sess=> sess.sid).map(sid => {
-        const item = get.bench.filter(entry=>entry.sid === sid)[0]
-        return item.reps.reduce((a,v,i)=> a + v*item.mass[i])})
-    const labels = get.date.filter(sess=>sess.exercises.includes("bench")).map(sess=>new Date(sess.date).toISOString())
-    const values = totalReps
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "go",
-        data: values,
-        borderColor: "rgb(75, 192, 192)",
-        fill: false,
-      },
-      /* {
-        label: "go",
-        data: values,
-        borderColor: "rgb(75, 192, 192)",
-        fill: false,
-      }, */
-    ],
-  };
+  const [input, setInput] = useState({interval: "SESSION", exercise: "ALL"});
 
-  const options = {
-    responsive: true,
-    interaction: {
-      mode: 'index',
-      intersect: false,
-    },
-    stacked: false,
-    plugins: {
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart - Multi Axis',
+  function optionsInitialiser(interval) {
+    let x
+    if (interval === "SESSION") x = {
+      type: "time",
+      time: {
+        unit: "day",
       },
-    },
-    scales: {
-      x: {
-        type: 'time',
-        time: {
-          unit: 'day'
-        }
+      grid: {
+        drawOnChartArea: false,
       },
-      y: {
-        type: 'linear',
-        display: true,
-        position: 'left',
+    }
+    else if (interval !== "SESSION") x = {
+      type: "category",
+      grid: {
+        drawOnChartArea: false,
       },
-      /* y1: {
-        type: 'linear',
-        display: true,
-        position: 'right',
-        grid: {
-          drawOnChartArea: false,
+    }
+    return {
+      responsive: true,
+      interaction: {
+        mode: "index",
+        intersect: false,
+      },
+      stacked: false,
+      plugins: {
+        title: {
+          display: false,
+          text: "Lifting Analysis",
         },
-      }, */
-    },
+      },
+      scales: {
+        x,
+        y1: {
+          type: "linear",
+          display: "auto",
+          position: "left",
+          grid: {
+            drawOnChartArea: true,
+          },
+          ticks: {
+            stepSize: 1,
+          },
+          grace: "5%",
+        },
+        y: {
+          type: "linear",
+          display: "auto",
+          position: "left",
+          grid: {
+            drawOnChartArea: false,
+          },
+          ticks: {
+            callback: function (value, index, ticks) {
+              return value + ` kg`;
+            },
+          },
+        },
+      },
+    }
   }
-/* 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-        display: false,
-      },
-      title: {
-        display: true,
-        text: "Bench",
-      },
-    },
-  };
- */
+
+    const dataInitialiser = useMemo(()=> { 
+      function getCustomInterval(date) {
+        const time = new Date(date);
+        const onejan = new Date(time.getFullYear(), 0, 1);
+        const referenceDate = /* begin ? new Date(begin) : */ onejan;
+        const intervalLengthInDays = 7
+        const interval = Math.ceil(
+          (time.getTime() - referenceDate.getTime()) /
+            86400000 /
+            intervalLengthInDays
+        );
+        return interval;
+      }
+      const totalWhateverIWant = (what, input) => {
+        if (input.exercise === "ALL") {
+          const output = get.date
+            .map((sess) => sess.sid)
+            .map((sid) => {
+              const exerciseCall = get.date
+                .filter((sess) => sess.sid === sid)
+                .map((v) => v.exercises)[0];
+              return exerciseCall
+                .map((exercise) => {
+                  const item = get[exercise].find(
+                    (entry) => entry.sid === sid
+                  );
+                  if (what === "mass") {
+                    return item.reps.reduce((a, v, i) => a + v * item.mass[i]);
+                  }
+                  else if (what === "reps")
+                    return item.reps.reduce((a, v) => {
+                      return a + v;
+                    });
+                  else if (what === "sets") return item.reps.length;
+                  else throw Error
+                })
+                .reduce((a, v) => {
+                  return a + v;
+                }, 0);
+            });
+            return output;
+          } else if (input.interval === "SESSION")
+          return get.date
+            .filter((sess) => sess.exercises.includes(input.exercise))
+            .map((sess) => sess.sid)
+            .map((sid) => {
+              const item = get[input.exercise].find((entry) => entry.sid === sid);
+              if (what === "mass")
+                return item.reps.reduce((a, v, i) => a + v * item.mass[i]);
+              else if (what === "reps") return item.reps.reduce((a, v) => a + v);
+              else if (what === "sets") return item.reps.length;
+              else throw Error
+            });
+        else if (input.interval === "MONTH") {
+          const sidsTagged = get.date
+            .filter(sess => sess.exercises.includes(input.exercise))
+            .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+            .map((v) => {
+              return {
+                sid: v.sid,
+                interval: `${new Date(v.date).getMonth() + 1}-${new Date(
+                  v.date
+                ).getFullYear()}`,
+              };
+            });
+    
+          const sidsBinned = sidsTagged.reduce((acc, v) => {
+            return Object.keys(acc).includes(v.interval)
+              ? { ...acc, [v.interval]: [...acc[v.interval], v.sid] }
+              : { ...acc, [v.interval]: [v.sid] };
+          }, {});
+          return Object.keys(sidsBinned)
+            .map(interval => sidsBinned[interval]
+            .reduce((acc, sid) => {
+              const exerciseObject = get[input.exercise].find((entry)=> entry.sid === sid)
+              if (what === "mass")
+                return acc + exerciseObject.reps.reduce((a, v, i) => a + v * exerciseObject.mass[i]);
+              else if (what === "reps") return acc + exerciseObject.reps.reduce((a, v) => a + v);
+              else if (what === "sets") return acc + exerciseObject.reps.length;
+              else throw Error
+            },0))
+        }
+        else if (input.interval === "WEEK") {
+          
+          const sidsTagged = get.date
+            .filter(sess => sess.exercises.includes(input.exercise))
+            .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+            .map((v) => {
+              return {
+                sid: v.sid,
+                interval: getCustomInterval(v.date)
+              }
+            });
+    
+          const difference = sidsTagged[sidsTagged.length - 1].interval - sidsTagged[0].interval
+          const categoryObject = {}
+          const categoryArray = Array(difference + 1).fill(null).map((v,i)=> sidsTagged[0].interval + i)
+          categoryArray.forEach(key => categoryObject[key] = sidsTagged.filter(val => val.interval === key && val.sid))
+    
+          console.log(categoryArray)
+    
+    
+          /* const sidsBinned = sidsTagged.reduce((acc, v) => {
+            return Object.keys(acc).includes(v.interval)
+              ? { ...acc, [v.interval]: [...acc[v.interval], v.sid] }
+              : { ...acc, [v.interval]: [v.sid] };
+          }, {}); */
+          return Object.values(categoryArray).map(array => array
+            .reduce((acc, sid) => {
+              const exerciseObject = get[input.exercise].find((entry)=> entry.sid === sid)
+              if (what === "mass")
+                return acc + exerciseObject.reps.reduce((a, v, i) => a + v * exerciseObject.mass[i]);
+              else if (what === "reps") return acc + exerciseObject.reps.reduce((a, v) => a + v);
+              else if (what === "sets") return acc + exerciseObject.reps.length;
+              else throw Error
+            },0))
+        }
+      };
+      const getLabels = (input) => {
+        if (input.exercise === "ALL" && input.interval === "SESSION") {
+          return get.date.map((sess) => new Date(sess.date).toISOString());
+        } else if (input.exercise !== "ALL" && input.interval === "SESSION") {
+          return get.date
+            .filter((sess) => sess.exercises.includes(input.exercise))
+            .map((sess) => new Date(sess.date).toISOString());
+        } else if (input.interval === "MONTH") {
+          const sorted = get.date.sort((a, b) => new Date(a) - new Date(b));
+          return [new Date(sorted[0].date).getMonth() + 1, new Date(sorted[sorted.length - 1].date).getMonth()+1]
+        } else if (input.interval === "WEEK") {
+          const sorted = get.date.sort((a, b) => new Date(a) - new Date(b));
+          const difference = getCustomInterval(sorted[sorted.length - 1].date) - getCustomInterval(sorted[0].date)
+          const output = Array(difference + 1).fill(null).map((v,i)=> getCustomInterval(sorted[0].date) + i)
+    
+          return output
+        }
+      };
+      return {
+      labels: getLabels(input),
+      datasets: [
+        {
+          yAxisID: "y",
+          xAxisID: "x",
+          label: "Tonnage (kg)",
+          data: totalWhateverIWant("mass", input),
+          borderColor: "rgb(75, 192, 192)",
+          backgroundColor: "rgb(75, 192, 192)",
+          fill: false,
+        },
+        {
+          yAxisID: "y1",
+          label: "Reps",
+          data: totalWhateverIWant("reps", input),
+          borderColor: "rgb(255, 192, 192)",
+          backgroundColor: "rgb(255, 192, 192)",
+          fill: false,
+        },
+        {
+          yAxisID: "y1",
+          label: "Sets",
+          data: totalWhateverIWant("sets", input),
+          borderColor: "rgb(70, 255, 192)",
+          backgroundColor: "rgb(70, 255, 192)",
+          fill: false,
+        },
+      ],
+    }}, [input, get])
+
+  const [data, setData] = useState(dataInitialiser);
+  const [options, setOptions] = useState(optionsInitialiser(input.interval))
+
+  useEffect(() => {
+    setData(dataInitialiser);
+    setOptions(optionsInitialiser(input.interval))
+  }, [input, dataInitialiser]);
+
   return (
     <div>
+      <button onClick={()=> console.log(input)}>input</button>
+      <fieldset>
+        <label>
+          Interval
+          <select onChange={(e) => setInput({...input, interval: e.target.value})}>
+            <option>SESSION</option>
+            <option>MONTH</option>
+            <option>WEEK</option>
+          </select>
+        </label>
+        <label>
+          Exercise
+          <select onChange={(e) => setInput({...input, exercise: e.target.value})}>
+            <option value="ALL">All</option>
+            <option value="deadlift">Deadlift</option>
+            <option value="squat">Squat</option>
+            <option value="bench">Bench</option>
+          </select>
+        </label>
+      </fieldset>
       <Line options={options} data={data} />
     </div>
   );
--- Client - Source/src/Tonnage.js
+++ Client - Source/src/Tonnage.js
@@ -3,13 +3,12 @@ import Graph from "./Graph";
 import returnTonnage from "./utils/tonnageFunctions";
 
 export default function Tonnage({ get }) {
+  const [page, setPage] = useState("TABLE");
   const [interval, setInterval] = useState(null);
   const [intervalLength, setIntervalLength] = useState([null, null]);
   const [referenceDate, setReferenceDate] = useState(null);
   const [showZeroes, setShowZeroes] = useState(false);
   const [variationFilter, setVariationFilter] = useState({});
-  // const customInput = useRef()
-
 
   const variationObject = {
     deadlift: [
@@ -23,100 +22,137 @@ export default function Tonnage({ get }) {
     ],
   };
 
-  return (
-    <>
-      <fieldset>
-        <label htmlFor="tonnageInterval">Interval</label>
-        <select
-          id="tonnageInterval"
-          onChange={(e) => setInterval(e.target.value)}
+  function returnPageButtons() {
+    return (
+      <>
+        <button
+          onClick={() =>
+            page === "TABLE" ? setPage("GRAPH") : setPage("TABLE")
+          }
         >
-          {/* <option value="">___</option> */}
-          <option value="ALL">All Time</option>
-          <option value="WEEK">Week</option>
-          <option value="MONTH">Month</option>
-          <option value="SESSION">Session</option>
-          <option value="CUSTOM">Custom</option>
-        </select>
-        {interval === "CUSTOM" && (
-          <>
-            <label htmlFor={`IntervalLength`}>Interval Length</label>
-            <input
-              id={`IntervalLength`}
-              placeholder={intervalLength[1] === "WEEKS" ? "1" : "7"}
-              onChange={(e) =>
-                intervalLength[1] === "WEEKS"
-                  ? setIntervalLength([
-                      parseInt(e.target.value) * 7,
-                      intervalLength[1],
-                    ])
-                  : setIntervalLength([
-                      parseInt(e.target.value),
-                      intervalLength[1],
-                    ])
-              }
-            />
-            <label htmlFor={`intervalLengthIn...`}>in...</label>
-            <select
-              id={`intervalLengthIn...`}
-              onChange={(e) =>
-                setIntervalLength([intervalLength[0], e.target.value])
-              }
-            >
-              <option value="DAYS">DAYS</option>
-              <option value="WEEKS">WEEKS</option>
-            </select>
-            <label htmlFor={`BeginReferenceDate`}>
-              Beginning Reference Date
-            </label>
-            <input
-              id={`BeginReferenceDate`}
-              type="date"
-              onChange={(e) => setReferenceDate(e.target.value)}
-            />
-          </>
-        )}{ interval !== "SESSION" &&
-        <>
-        <label htmlFor="showZeroes">Show Zeroes</label>
-        <input
-          type="checkbox"
-          id="showZeroes"
-          onChange={(e) => setShowZeroes(e.target.checked)}
-        />
-        </>}
-        {["deadlift", "squat", "bench"].map((exercise) => {
-          return (
-            <div key={`${exercise}VariationFilter`} style={{display: "inline-block"}}>
-              <label htmlFor={`${exercise}VariationFilter`}>
-                {exercise.replace(/^\w/, exercise[0].toUpperCase())}:
-              </label>
-              <select
-                id={`${exercise}VariationFilter`}
+          {page === "TABLE" ? `View Graph` : `View Table`}
+        </button>
+      </>
+    );
+  }
+
+  function returnTable() {
+    return (
+      <>
+        <fieldset>
+          <label htmlFor="tonnageInterval">Interval</label>
+          <select
+            id="tonnageInterval"
+            onChange={(e) => setInterval(e.target.value)}
+          >
+            {/* <option value="">___</option> */}
+            <option value="ALL">All Time</option>
+            <option value="WEEK">Week</option>
+            <option value="MONTH">Month</option>
+            <option value="SESSION">Session</option>
+            <option value="CUSTOM">Custom</option>
+          </select>
+          {interval === "CUSTOM" && (
+            <>
+              <label htmlFor={`IntervalLength`}>Interval Length</label>
+              <input
+                id={`IntervalLength`}
+                placeholder={intervalLength[1] === "WEEKS" ? "1" : "7"}
                 onChange={(e) =>
-                  setVariationFilter({
-                    ...variationFilter,
-                    [exercise]: e.target.value,
-                  })
+                  intervalLength[1] === "WEEKS"
+                    ? setIntervalLength([
+                        parseInt(e.target.value) * 7,
+                        intervalLength[1],
+                      ])
+                    : setIntervalLength([
+                        parseInt(e.target.value),
+                        intervalLength[1],
+                      ])
+                }
+              />
+              <label htmlFor={`intervalLengthIn...`}>in...</label>
+              <select
+                id={`intervalLengthIn...`}
+                onChange={(e) =>
+                  setIntervalLength([intervalLength[0], e.target.value])
                 }
               >
-                <option value=""> Use All </option>
-                {variationObject[exercise].flat().map((value) => {
-                  return <option key={`${value}`}>{value}</option>;
-                })}
+                <option value="DAYS">DAYS</option>
+                <option value="WEEKS">WEEKS</option>
               </select>
-            </div>
-          );
-        })}
-      </fieldset>
-      {returnTonnage(
-        get,
-        interval || "ALL",
-        intervalLength[0] || 7,
-        referenceDate,
-        showZeroes,
-        variationFilter
-      )}
-      <Graph get={get}/>
-    </>
+              <label htmlFor={`BeginReferenceDate`}>
+                Beginning Reference Date
+              </label>
+              <input
+                id={`BeginReferenceDate`}
+                type="date"
+                onChange={(e) => setReferenceDate(e.target.value)}
+              />
+            </>
+          )}
+          {interval !== "SESSION" && (
+            <>
+              <label htmlFor="showZeroes">Show Zeroes</label>
+              <input
+                type="checkbox"
+                id="showZeroes"
+                onChange={(e) => setShowZeroes(e.target.checked)}
+              />
+            </>
+          )}
+          {["deadlift", "squat", "bench"].map((exercise) => {
+            return (
+              <div
+                key={`${exercise}VariationFilter`}
+                style={{ display: "inline-block" }}
+              >
+                <label htmlFor={`${exercise}VariationFilter`}>
+                  {exercise.replace(/^\w/, exercise[0].toUpperCase())}:
+                </label>
+                <select
+                  id={`${exercise}VariationFilter`}
+                  onChange={(e) =>
+                    setVariationFilter({
+                      ...variationFilter,
+                      [exercise]: e.target.value,
+                    })
+                  }
+                >
+                  <option value=""> Use All </option>
+                  {variationObject[exercise].flat().map((value) => {
+                    return <option key={`${value}`}>{value}</option>;
+                  })}
+                </select>
+              </div>
+            );
+          })}
+        </fieldset>
+        {returnTonnage(
+          get,
+          interval || "ALL",
+          intervalLength[0] || 7,
+          referenceDate,
+          showZeroes,
+          variationFilter
+        )}
+      </>
+    );
+  }
+
+  function returnPage() {
+    if (page === "TABLE") return returnTable();
+    else if (page === "GRAPH")
+      return (
+        <div style={{ width: "50%" }}>
+          <Graph get={get} />
+        </div>
+      );
+  }
+
+  return (
+    <div>
+      {returnPageButtons()}
+      {returnPage()}
+    </div>
   );
 }
