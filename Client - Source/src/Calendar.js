1438ad08ec346cab01096f5bf0172d7a232a9dcf
code <codecode60@protonmail.com>
2022-08-15 23:55:27 +0930

    Building out Calendar

--- Client - Source/src/Calendar.js
+++ Client - Source/src/Calendar.js
@@ -1,50 +1,94 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-export default function Calendar({get}) {
-    const svgRef = useRef()
-    const data = get.date.map(sess =>sess.date.slice(0,10))
-    useEffect(() => {
-        const rectSize = 30 
-        const dimensions = {
-            width: 800,
-            height: 600
-        }
-        const svg = d3.select(svgRef.current)
-                        .attr('width', dimensions.width)
-                        .attr('height', dimensions.height)
-                        .style('background-color', 'grey')
-        const x = d3.scaleLinear()
-                    .domain([0, data.length])
-                    .range([0, dimensions.width])
-        svg.append('g')
-            .selectAll('text')
-            .data(data)
-            .join('text')
-            .text(d => `${d}`)
-            .attr('x', (d,i) => x(i) + 2)
-            .attr('y', dimensions.height - 2)
-            .attr('fill', 'white')
-            .style('font-size', '8px')
+export default function Calendar({ get }) {
+  const svgRef = useRef();
+  const sortedSessions = get.date.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+  const mostRecentSessionDate = new Date(sortedSessions.at(-1).date);
+  const sessionsForMostRecentMonth = get.date
+    .filter((sess) => {
+      const sessionDate = new Date(sess.date);
+      return (
+        sessionDate <=
+          new Date(
+            mostRecentSessionDate.getFullYear(),
+            mostRecentSessionDate.getMonth() + 1,
+            1
+          ) &&
+        sessionDate >=
+          new Date(
+            mostRecentSessionDate.getFullYear(),
+            mostRecentSessionDate.getMonth(),
+            1
+          )
+      );
+    })
+    .map((sess) => sess.date);
 
-        data.forEach((date, i) => {
-            svg.append('g')
-                .selectAll('rect')
-                .data(Array(14))
-                .join('rect')
-                .attr('x', x(i) + 0.5*rectSize)
-                .attr('y', (d, j) => j * (dimensions.height / 14) + 2)
-                .attr('width', rectSize)
-                .attr('height', rectSize)
-                .attr('fill', 'red')
-        })  
-    }, [data])
-    
-    return (
-        <div>
-            <svg ref={svgRef}>
+  const data = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
 
-            </svg>
-        </div>
-    )
+  useEffect(() => {
+    const rectSize = 80;
+    const dimensions = {
+      width: 800,
+      height: 600,
+    };
+    const svg = d3
+      .select(svgRef.current)
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
+      .style("background-color", "grey");
+    const x = d3
+      .scaleLinear()
+      .domain([0, data.length])
+      .range([0, dimensions.width]);
+    const y = d3
+      .scaleLinear()
+      .domain([0, data.length])
+      .range([0, dimensions.height]);
+
+    svg
+      .append("g")
+      .selectAll("text")
+      .data(data)
+      .join("text")
+      .text((d) => `${d}`)
+      .attr("x", (d, i) => x(i))
+      .attr("y", 50)
+      .attr("fill", "black")
+      .style("font-size", "19px");
+
+    data.forEach((date, i) => {
+      svg
+      .append("g")
+      .selectAll("text")
+      .data(Array(6))
+      .join("text")
+      .text((d, ind) => `${((28 + i + ind * 7) % 31 + 1)}`)
+      .attr("x", x(i))
+      .attr("y", (d, j) => j * (rectSize +10) + 100)
+      .attr("fill", "black")
+      .style("font-size", "19px")
+        /* .append("g")
+        .selectAll("rect")
+        .data(Array(5))
+        .join("rect")
+        .attr("x", x(i))
+        .attr("y", (d, j) => j * (rectSize + 20) + 100)   
+        .attr("width", rectSize)
+        .attr("height", rectSize)
+        .attr("fill", (d, ind) => i % 2 === 0 ? 'red' : ind % 2 === 0 ? 'orange' : 'transparent')
+        .attr("stroke-width", 2)
+        .attr("stroke", (d, ind) => i % 2 === 0 ? 'black' : ind % 2 === 0 ? 'black' : 'transparent') */
+    });
+  }, [data]);
+
+  return (
+    <div>
+      {console.log(data)}
+      <svg ref={svgRef}></svg>
+    </div>
+  );
 }
