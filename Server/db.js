1206aa458fa4f414120cc9098a4d6763e38b756a
code <codecode60@protonmail.com>
2022-08-21 17:34:25 +0930

    small changes

--- Server/db.js
+++ Server/db.js
@@ -30,14 +30,14 @@ const url = require('url')
 
 const params = url.parse(process.env.DATABASE_URL);
 const auth = params.auth.split(':');
-
+console.log(params, auth)
 const configHEROKU = {
   user: auth[0],
   password: auth[1],
   host: params.hostname,
   port: params.port,
   database: params.pathname.split('/')[1],
- // ssl: true
+  ssl: true
 };
 
 // const pool = new Pool(configLOCAL);
