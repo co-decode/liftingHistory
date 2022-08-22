67d4e2dc3f35a49f38cffe01afb278ab1340b834
code <codecode60@protonmail.com>
2022-08-22 14:02:12 +0930

    end slash on backend url injection, removed and fixed

--- Client - Source/src/utils/variables.js
+++ Client - Source/src/utils/variables.js
@@ -3,5 +3,5 @@ export const authenticatedKick = '/log'
 export const exerciseArray = ["deadlift", "squat", "bench"]
 // ^^ Haven't decided whether I'll be using this yet.
 
-export const backend = "https://node-lifting-history2.herokuapp.com/"
+export const backend = "https://node-lifting-history2.herokuapp.com"
 // backend = "http://localhost:3001"
