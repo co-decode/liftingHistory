a142b17680ce40ab8c98a8a14a2410793dea2e56
code <codecode60@protonmail.com>
2022-08-24 16:21:55 +0930

    forward slash error again

--- Client - Source/src/utils/variables.js
+++ Client - Source/src/utils/variables.js
@@ -3,6 +3,6 @@ export const authenticatedKick = '/log'
 export const exerciseArray = ["deadlift", "squat", "bench"]
 // ^^ Haven't decided whether I'll be using this yet.
 
-export const backend = "https://lifting-history-2-container.herokuapp.com/"
+export const backend = "https://lifting-history-2-container.herokuapp.com"
 // export const backend = "https://node-lifting-history2.herokuapp.com"
 // backend = "http://localhost:3001"
